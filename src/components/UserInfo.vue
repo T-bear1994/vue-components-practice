752ee7fb302187e625abbc12009ae2bbebee4b4f
Takahiro Iizuka <agdjgt@gmail.com>
2022-10-08 21:17:23 +0900

    first commit

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <base-card>
+      <template #header>
+        <header>
+          <h3>{{ fullName }}</h3>
+          <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+        </header>
+      </template>
+      <template #default>
+        <p>{{ infoText }}</p>
+      </template>
+    </base-card>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['fullName', 'infoText', 'role'],
+};
+</script>
+
+<style scoped>
+section header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
